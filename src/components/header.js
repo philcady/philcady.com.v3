2ff4cd68c9e59f6b8cd1bc8b94e429051ba97284
Phil Cady <phillip.m.cady@gmail.com>
2020-08-18 10:55:19 -0700

    added router

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import Logo from './logo';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function Header(params) {
   return(
     <header className="App-header">
+      <nav>
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+      </nav>
       <Logo />
     </header>
   )
+}
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+    </div>
+  );
 }
 
 export default Header;
