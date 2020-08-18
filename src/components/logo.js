9b46528b69a2464be91eb4c5badbfeb82aeef206
Phil Cady <phillip.m.cady@gmail.com>
2020-08-18 15:39:45 -0700

    added fonts

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Logo() {
   return (
-    <div className="App-logo">
+    <div className="logo">
       <span className="sr-only">Phil Cady - Web Developer</span>
     </div>
   )
