03d64b76663a22dcecb7b2dacb05590ac05f5dbb
Phil Cady <phillip.m.cady@gmail.com>
2020-08-17 17:18:13 -0700

    added stuff

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from './logo';
+
+function Header(params) {
+  return(
+    <header className="App-header">
+      <Logo />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="App-logo">
+      <span>Phil Cady - Web Developer</span>
+      </div>
+  )
+}
+
+export default Logo;
