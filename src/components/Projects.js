028ce2d32af1065da4615699e696ca6a114937ac
Phil Cady <phillip.m.cady@gmail.com>
2020-08-19 16:22:17 -0700

    still trying to fix the projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,28 +2,33 @@ import React from "react"
 import Modal from './Modal'
 import useModal from './useModal'
 
-const projArr = ['hello jeanine', 'hello navin']
+const projArr = [
+  {id:1, title: 'VW', thumb: '../static/media/vw_beetle.png', alt:'VW Beetle'},
+  {id:2, title: 'Penny Mac', thumb: '../static/media/penny_mac.71973829.png', alt: 'Penny Mac'},
+]
 
 export default function Projects() {
   const {isShowing, toggle} = useModal();
+
   return (
       <div className="projects-container">
         <Modal
           isShowing={isShowing}
           hide={toggle}
         >
-          {projArr[1]}
+          {
+            <>
+              <h1>{projArr[1].title}</h1>
+              <img src={projArr[1].thumb} alt={projArr[1].alt} />
+            </>
+          }
 
         </Modal>
-          <div className="project vw">
-            <h1 className="title">VW</h1>
+          <div className="project vw" onClick={toggle}>
           </div>
-          <div className="project penny-mac">
-            <h1 className="title">Penny Mac</h1>
-            <button className="button-default" onClick={toggle}>Show Modal</button>
+          <div className="project penny-mac" onClick={toggle}>
           </div>
-          <div className="project city-natl-bank">
-            <h1 className="title">City National Bank</h1>
+          <div className="project city-natl-bank" onClick={toggle}>
           </div>
       </div>
   )
